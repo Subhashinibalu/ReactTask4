58bba4bc892fd1619eead21fd74907d4d5bdfebe
Subhashinibalu <subhashiniulk@gmail.com>
2024-05-09 11:29:35 +0530

    Updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,24 +4,32 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     return (
         <div>
-            <ul className="nav justify-content-center mt-5 ">
-  <li className="nav-item">
-    <Link to ="/">ALL</Link>
-  </li>
-  <li className="nav-item">
-  <Link to ="/fullstack">FULL STACK DEVELOPMENT</Link>
-  </li>
-  <li className="nav-item">
-  <Link to ="/datascience">DATA SCIENCE</Link>
-  </li>
-  <li className="nav-item">
+            <nav className="navbar navbar-expand-lg  mt-5">
+  <div className="container-fluid ">
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
+      <ul className="navbar-nav ">
+        <li className="nav-item">
+        <Link to ="/">ALL</Link>
+        </li>
+        <li className="nav-item">
+        <Link to ="/fullstack">FULL STACK DEVELOPMENT</Link>
+        </li>
+        <li className="nav-item">
+        <Link to ="/datascience">DATA SCIENCE</Link>
+        </li>
+        <li className="nav-item">
   <Link to ="/cybersecurity">CYBER SECURITY</Link>
   </li>
   <li className="nav-item">
   <Link to ="/career">CAREER</Link>
   </li>
-  
-</ul>
+      </ul>
+    </div>
+  </div>
+</nav>
 <hr/>
         </div>
     );
